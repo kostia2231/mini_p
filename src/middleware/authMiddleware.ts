01ad5bd16b176de14be03e059754794fd35222dd
konstantin <kostia505@gmail.com>
2024-12-18 12:12:19 +0100

    middleware: authMWare added

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+const SECRET_KEY = process.env.SECRET_KEY;
+
+interface JWTPayload {
+  id: string;
+}
+
+const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    res.status(401).json({ message: "token is required", success: false });
+    return;
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY!) as JWTPayload;
+    req.user = decoded.id;
+    next();
+  } catch (e) {
+    res.status(401).json({ message: "invalid token", success: false });
+  }
+};
