3f22772703a06982d4e55d2dce8502f16849d893
konstantin <kostia505@gmail.com>
2024-12-19 11:36:15 +0100

    task model: added

---
+++ src/models/Task.ts
@@ -0,0 +1,31 @@
+import mongoose, { model, Schema, Document } from "mongoose";
+
+type Status = "todo" | "in-progress" | "done";
+type Priority = "low" | "medium" | "high";
+
+interface ITask extends Document {
+  title: string;
+  description: string;
+  status: Status;
+  priority: Priority;
+  userId: mongoose.Types.ObjectId;
+}
+
+const TaskSchema: Schema = new Schema<ITask>({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  status: {
+    type: String,
+    enum: ["todo", "in-progress", "done"],
+    default: "todo",
+  },
+  priority: {
+    type: String,
+    enum: ["low", "medium", "high"],
+    default: "medium",
+  },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+const Task = model<ITask>("Task", TaskSchema);
+export default Task;
